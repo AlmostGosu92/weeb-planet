5d0ebb33cbb1b7dd68fcdeab64da1fd123ca1087
Axel Eriksson <a.eriksson0303@gmail.com>
2019-10-26 18:01:01 +0200

    fixing filepaths and added main method in GuesserGame.java

---
+++ src/GuesserGame/GuesserGame.java
@@ -0,0 +1,10 @@
+package GuesserGame;
+
+public class GuesserGame {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- src/GuesserGame/GuesserTest.java
+++ src/GuesserGame/GuesserTest.java
@@ -1,9 +1,10 @@
+package GuesserGame;
 
 public class GuesserTest {
 
 	public static void main(String[] args) {
 		String s = null;
-		if (s.equals("Barcelona")) {
+		if ("Barcelona".equals(s)) {
 			System.out.println("Ay, caramba");
 		}
 
